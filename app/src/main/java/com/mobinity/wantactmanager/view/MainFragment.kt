99a06e15a2ad86bbb5ce51900068fd711c087dfd
김명진 <mobinity.kmj@gmail.com>
2021-04-14 18:33:34 +0900

    [Add] 데이터바인딩 세팅, Splash 화면 추가, navigation graph 추가

---
+++ app/src/main/java/com/mobinity/wantactmanager/view/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.mobinity.wantactmanager.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.mobinity.wantactmanager.R
+import com.mobinity.wantactmanager.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
